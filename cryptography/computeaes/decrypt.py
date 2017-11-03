6160a71489d08da00cb55b3b77e174f47d46eee8
Akhil Seshan <akhilses@gmail.com>
2017-11-03 22:29:52 +0530

    Update decrypt.py

--- cryptography/computeaes/decrypt.py
+++ cryptography/computeaes/decrypt.py
@@ -7,8 +7,8 @@
 import base64
 from Crypto.Cipher import AES
 
-key = base64.b64decode("6v3TyEgjUcQRnWuIhjdTBA==")
-ciphertext = base64.b64decode("rW4q3swEuIOEy8RTIp/DCMdNPtdYopSRXKSLYnX9NQe8z+LMsZ6Mx/x8pwGwofdZ")
+key = base64.b64decode("r7y1dhmTvjQrcra7A1UQFw==")
+ciphertext = base64.b64decode("V3Vqirostg6qW26sle5mnyrwEYSrteN6oHkilO50e9dFkN+0JhC3yu0LcQNw/hXU")
 crypter = AES.new(key, AES.MODE_ECB)
 plaintext = crypter.decrypt(ciphertext).decode("utf-8")
 
